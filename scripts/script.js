595f492df56266d9017e34651c351b2dfe914ebf
DanielleIsMe <danielle.oconnellp@gmail.com>
2025-01-16 20:30:26 +1100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+//SHOW INDENTPs
+function showTas() {
+    const dropCont = document.getElementById("tasIndent");
+
+    dropCont.classList.toggle("dropContent2");
+}
+function showSal() {
+    const dropCont = document.getElementById("salIndent");
+
+    dropCont.classList.toggle("dropContent2");
+}
+function showAlo() {
+    const dropCont = document.getElementById("aloIndent");
+
+    dropCont.classList.toggle("dropContent2");
+}
