de0aa407c33fce2840c04766f546cf6d6ce3c601
DanielleIsMe <danielle.oconnellp@gmail.com>
2025-01-16 22:35:35 +1100

    show/hide functionality for tassalyr.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,31 @@
 //SHOW INDENTPs
-function showTas() {
-    const dropCont = document.getElementById("tasIndent");
+function showI1() {
+    const dropCont = document.getElementById("indent1");
 
     dropCont.classList.toggle("dropContent2");
 }
-function showSal() {
-    const dropCont = document.getElementById("salIndent");
+function showI2() {
+    const dropCont = document.getElementById("indent2");
 
     dropCont.classList.toggle("dropContent2");
 }
-function showAlo() {
-    const dropCont = document.getElementById("aloIndent");
+function showI3() {
+    const dropCont = document.getElementById("indent3");
+
+    dropCont.classList.toggle("dropContent2");
+}
+function showI4() {
+    const dropCont = document.getElementById("indent4");
+
+    dropCont.classList.toggle("dropContent2");
+}
+function showI5() {
+    const dropCont = document.getElementById("indent5");
+
+    dropCont.classList.toggle("dropContent2");
+}
+function showI6() {
+    const dropCont = document.getElementById("indent6");
 
     dropCont.classList.toggle("dropContent2");
 }
